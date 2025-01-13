4e5d8da778c8abf2d2498fac2b43b8216d9eca38
bain-liao <liao.bain@mths.ca>
2025-01-13 11:15:30 -0500

    Created the code for my servo motor in my final project

--- main.ts
+++ main.ts
@@ -1,8 +1,28 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Bain Liao
+ * Created on: Jan 2025
+ * This program controls the rubber band launcher servo
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(servoNumber1, -15)
+    basic.clearScreen()
+    basic.showString('Reset')
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 50)
+    basic.clearScreen()
+    basic.showString('Launch!')
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
